b2483ccff72615be15cc0e38ade1a1e10edfe741
Nenad Curcic <nenad.curcic@gamecredits.com>
2019-10-05 09:48:53 +0200

    Initial commit

---
+++ Blumind Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blumind_Demo.MvcApplication" Language="C#" %>
---
+++ Blumind Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blumind_Demo.Startup))]
+namespace Blumind_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
